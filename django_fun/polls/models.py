296044f46a83be6f880378c19eccc0742a1cfd63
zaddmc <davidbk927@gmail.com>
2025-09-16 15:39:18 +0200

    admin site is improved

--- django_fun/polls/models.py
+++ django_fun/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -8,8 +9,10 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    @admin.display(boolean=True, ordering="pub_date", description="Published Recently?")
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
