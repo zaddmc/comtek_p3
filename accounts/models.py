e9a742aad38b715ccac9793df38e5d1318a467a5
Hoothoot5321 <martinjens986@gmail.com>
2025-10-13 09:29:12 +0200

    made som refactorin

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,11 @@ import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 
 class User(AbstractUser):
+    """
+    Custom user model so uuid is used instead of ID 
+    """
     uuid = models.UUIDField(default=uuid.uuid4,editable=False,unique=True,primary_key=True)
     pass
 
--- suitcases/views.py
+++ suitcases/views.py
@@ -1,11 +1,8 @@
-from hashlib import sha256
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Suitcase, SuitcaseBleInfo
 from .forms import RentForm
 from django.http import HttpRequest,HttpResponse, HttpResponseRedirect  
 from http import HTTPStatus
-import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 
@@ -20,85 +17,69 @@ class SuitcaseDisplayView(LoginRequiredMixin,DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        context["renter_uuid"] = "" 
-        if(self.object.rented):
-            renter_uuid = self.object.rented_by.uuid
-            context["renter_uuid"] = renter_uuid 
-
+        renter_uuid = self.object.rented_by.uuid
+        context["renter_uuid"] = renter_uuid
         context["form"] = RentForm()
-
-        if(context["renter_uuid"] == self.request.user.uuid):
-            context["form"] = ""
-
         context["user_uuid"] = self.request.user.uuid 
-        context["encoded_key"] = "null"
 
-        if hasattr(self.object,"suitcasebleinfo"):
-            suitcase_ble_info:SuitcaseBleInfo = self.object.suitcasebleinfo
-            m = sha256()
-            m.update((str(suitcase_ble_info.secret_key) + str(suitcase_ble_info.msg_count)).encode())
-            context["encoded_key"] = m.hexdigest()
+        context["encoded_key"] = None
+        if not hasattr(self.object,"suitcasebleinfo"):
+            return context
+        context["encoded_key"] = self.object.suitcasebleinfo.get_hashed_secret_key() 
 
         return context
 
-    def handle_put(self,request:HttpRequest,uuid:str):
-
-        suitcase:Suitcase = Suitcase.objects.get(uuid=uuid)
-
-        if(suitcase.rented):
-            if(suitcase.rented_by.uuid != self.request.user.uuid):
-                return HttpResponse("You are not allowed to unrent this >(".encode(),HTTPStatus.BAD_REQUEST)
-
-        suitcase.rented = False
-        suitcase.rented_by = None 
-        suitcase.rented_date= None 
-        suitcase.expiration_date= None 
-
-        suitcase.save()
-
-        return HttpResponseRedirect(request.path)
     def get(self, request:HttpRequest, uuid:str):
 
         suitcase:Suitcase = Suitcase.objects.get(uuid=uuid)
-        if(suitcase.rented):
+        if(suitcase.rented_by and suitcase.rented):
             if(suitcase.rented_by.uuid != self.request.user.uuid):
                 return HttpResponse("You are not allowed to see this >(".encode(),HTTPStatus.BAD_REQUEST)
                 
-        self.object = suitcase  # Set self.object explicitly here
+        self.object = suitcase  
         return render(request,self.template_name,self.get_context_data())
+
     def post(self,request:HttpRequest,uuid:str):
+
+        #Man kan ikke lave put request med post form i html
+        #så vi laver en field i post request hvor vi kan
+        #specificere hvad for en method det er
         real_method = request.POST.get("_method")
         if(real_method == "put"):
             return self.handle_put(request,uuid)
-        post_form = RentForm(request.POST)
 
+        post_form = RentForm(request.POST)
         if not post_form.is_valid():
             return HttpResponse("Invalid form".encode(),HTTPStatus.BAD_REQUEST)
 
         suitcase:Suitcase = Suitcase.objects.get(uuid = uuid)
         if(suitcase.rented):
             messages.error(request,"Suitcase is already rented") 
+            return HttpResponseRedirect(request.path)
 
+        rent_err = suitcase.rent(request.user,post_form)
+
+        if(rent_err):
+            messages.error(request,rent_err) 
             return HttpResponseRedirect(request.path)
 
 
-        suitcase.rented_by = self.request.user
-        suitcase.rented = True 
-
-        suitcase.rented_date = datetime.date.today()
-        suitcase.expiration_date = post_form.cleaned_data["expiration_date"]
-        if(suitcase.expiration_date <= suitcase.rented_date):
-            messages.error(request,"Expiration date must be after today") 
-            return HttpResponseRedirect(request.path)
-
-
-        suitcase.save()
-
+        #Ved ikke hvorfor men man skal åbenbart fetch suitcase igen med get
+        #efter man har brugt save.
+        #Prøv at fjerne linjen og se om det stadigvæk virker
         suitcase = Suitcase.objects.get(uuid=uuid)
-        self.object = suitcase  # Set self.object explicitly here
+        self.object = suitcase  
+        return HttpResponseRedirect(request.path)
 
+    def handle_put(self,request:HttpRequest,uuid:str):
+        suitcase:Suitcase = Suitcase.objects.get(uuid=uuid)
 
+        if(suitcase.rented):
+            if(suitcase.rented_by.uuid != self.request.user.uuid):
+                return HttpResponse("You are not allowed to unrent this >(".encode(),HTTPStatus.BAD_REQUEST)
+
+        suitcase.unrent()
+        suitcase.save()
 
         return HttpResponseRedirect(request.path)
 
