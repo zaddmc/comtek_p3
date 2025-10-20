5ce6af5c95ea45664ca6a4afd9df4093d3372896
Alehandre420 <alexandernikolajlarsen@gmail.com>
2025-10-20 10:53:55 +0200

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,4 +11,9 @@ class CustomUserCreate(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User 
         fields = ["username","password1","password2"]
-        
+        help_texts = {"username":None} # Remove username help text.
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password1"].help_text = ""
+        self.fields["password2"].help_text = ""
