dd4fbc2fe82f2e93a2c900889f53e05f10834bea
Hoothoot5321 <martinjens986@gmail.com>
2025-10-20 10:47:03 +0200

    created app specific templates

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+
+
+from django.urls import path
+
+from .views import HomeView
+
+urlpatterns = [
+        path("",HomeView.as_view(),name="front-page"),
+]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
+from django.http import request
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
 
-class HomeView(View):
-    pass
+class HomeView(TemplateView):
+    template_name = "home/index.html"
 
