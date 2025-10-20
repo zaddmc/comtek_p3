5c9e3dc25c6343e1faf1ec18276e008f4e7feeab
Hoothoot5321 <martinjens986@gmail.com>
2025-10-20 12:03:07 +0200

    til skibberen

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.http import request
 from django.shortcuts import render
 from django.views import View
-from django.views.generic.base import TemplateView
-
+from django.views.generic.base import TemplateView 
 class HomeView(TemplateView):
+
     template_name = "home/index.html"
+
 
