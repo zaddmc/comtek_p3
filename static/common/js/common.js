090d480b818a1150971ccd3704aeb677323b4d7a
Hoothoot5321 <martinjens986@gmail.com>
2025-10-22 15:22:26 +0200

    added dark mode and other thingys

--- static/common/js/common.js
+++ static/common/js/common.js
@@ -20,16 +20,17 @@ function setup() {
 
 
 	let figures = document.querySelectorAll(".figure");
-	let audio = document.getElementById("audio_tag");
 
+	console.log(figures.length);
+	let audio = document.getElementById("audio_tag");
 	for (let i = 0; i < figures.length; i++) {
 		figures[i].addEventListener("click", () => {
 			audio.pause();
 			audio.currentTime = 0;
 			audio.play();
-			figures[i].classList.remove("figure_go")
+			figures[i].classList.remove("figure-go")
 			void figures[i].offsetWidth;
-			figures[i].classList.add("figure_go")
+			figures[i].classList.add("figure-go")
 		});
 	}
 
