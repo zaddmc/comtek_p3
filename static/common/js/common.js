d64892d6d6e9d0b41519ede628bfe3317a7d6985
zaddmc <davidbk927@gmail.com>
2025-10-27 11:06:03 +0100

    why pause nothing, it only breaks zen, and how da fuck didnt you notice that, you moron

--- static/common/js/common.js
+++ static/common/js/common.js
@@ -1,39 +1,38 @@
 function setup() {
-	var toggle = document.getElementById("theme_toggle");
+  var toggle = document.getElementById("theme_toggle");
 
-	var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-	if (storedTheme)
-		document.documentElement.setAttribute('data-theme', storedTheme)
+  var storedTheme =
+    localStorage.getItem("theme") ||
+    (window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light");
+  if (storedTheme)
+    document.documentElement.setAttribute("data-theme", storedTheme);
 
+  toggle.onclick = function () {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
 
-	toggle.onclick = function() {
-		var currentTheme = document.documentElement.getAttribute("data-theme");
-		var targetTheme = "light";
+    if (currentTheme === "light") {
+      targetTheme = "dark";
+    }
 
-		if (currentTheme === "light") {
-			targetTheme = "dark";
-		}
+    document.documentElement.setAttribute("data-theme", targetTheme);
+    localStorage.setItem("theme", targetTheme);
+  };
 
-		document.documentElement.setAttribute('data-theme', targetTheme)
-		localStorage.setItem('theme', targetTheme);
-	};
+  let figures = document.querySelectorAll(".figure");
 
-
-	let figures = document.querySelectorAll(".figure");
-
-	console.log(figures.length);
-	for (let i = 0; i < figures.length; i++) {
-
-		let audio = figures[i].querySelector(".figure_audio");
-		figures[i].addEventListener("click", () => {
-			audio.pause();
-			audio.currentTime = 0;
-			audio.play();
-			figures[i].classList.remove("figure-go")
-			void figures[i].offsetWidth;
-			figures[i].classList.add("figure-go")
-		});
-	}
-
+  console.log(figures.length);
+  for (let i = 0; i < figures.length; i++) {
+    let audio = figures[i].querySelector(".figure_audio");
+    figures[i].addEventListener("click", () => {
+      audio.currentTime = 0;
+      audio.play();
+      figures[i].classList.remove("figure-go");
+      void figures[i].offsetWidth;
+      figures[i].classList.add("figure-go");
+    });
+  }
 }
 setup();
