3e47d06764405c9e2b7b7119232d9eafe0284a43
William Bjerglund <33435986+WilliamBjerglund@users.noreply.github.com>
2025-10-28 11:26:36 +0100

    str method added

--- suitcases/models.py
+++ suitcases/models.py
@@ -10,7 +10,7 @@ from suitcases.forms import RentForm
 class Category(models.Model):
     name = models.CharField(max_length=256)
 
-    def __str__(self): #TODO: remove method since it might break BLE
+    def __str__(self): #NOTE: This just displays name instead of UUID
         return self.name
 
 class Suitcase(models.Model):
@@ -25,6 +25,9 @@ class Suitcase(models.Model):
     name = models.CharField(max_length=20, unique=True)
     objects = models.Manager()
     categories = models.ManyToManyField(Category)
+
+    def __str__(self): #NOTE: This just displays name instead of UUID
+        return self.name
 
     def getCategories(self):
         return [category.name for category in self.categories.all()]
