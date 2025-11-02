ab3e7e5f973b40ae60b6a4cd75b6960093a6939a
William Bjerglund <33435986+WilliamBjerglund@users.noreply.github.com>
2025-11-02 15:28:58 +0100

    fixed width scaling

--- user/models.py
+++ user/models.py
@@ -2,29 +2,35 @@
 from django.db import models
 from suitcases.models import Category
 
+
 class QuizQuestion(models.Model):
-    question_text = models.CharField(max_length=255)
+    question_text = models.CharField(max_length=360)
     order = models.IntegerField(default=0)  # To control question order
     is_active = models.BooleanField(default=True)
-    
+
     class Meta:
-        ordering = ['order']
-    
+        ordering = ["order"]
+
     def __str__(self):
         return self.question_text
 
+
 class QuestionOption(models.Model):
-    question = models.ForeignKey(QuizQuestion, on_delete=models.CASCADE, related_name='options')
+    question = models.ForeignKey(
+        QuizQuestion, on_delete=models.CASCADE, related_name="options"
+    )
     option_text = models.CharField(max_length=255)
-    categories = models.ManyToManyField(Category, blank=True, related_name='question_options')
+    categories = models.ManyToManyField(
+        Category, blank=True, related_name="question_options"
+    )
     order = models.IntegerField(default=0)
-    
+
     class Meta:
-        ordering = ['order']
-    
+        ordering = ["order"]
+
     def __str__(self):
         return f"{self.question.question_text} - {self.option_text}"
-    
+
     def get_category_names(self):
-        """ Helper to get all category names """
+        """Helper to get all category names"""
         return [category.name for category in self.categories.all()]
