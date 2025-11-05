e8369d03ec7c9fe2c567d5fbbd98df0813889711
Hoothoot5321 <martinjens986@gmail.com>
2025-11-05 09:09:38 +0100

    updated user rent logic

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 # user/urls.py
 from django.urls import path
-from .views import NonRentedView, RecommendationView, RentedView, QuizView, clear_recommendations
 
+from home.views import WIPView
+from .views import NonRentedView, RecommendationView, RentedView, QuizView 
 urlpatterns = [
     path("", NonRentedView.as_view(), name="non-rented-view"),
     path("quiz/", QuizView.as_view(), name="quiz"),
     path("quiz/recommendations/", RecommendationView.as_view(), name="quiz-recommendations"),
     path("rented/", RentedView.as_view(), name="rented-view"),
-    path("clear-recommendations/", clear_recommendations, name="clear-recommendations"),
+    path("clear-recommendations/", WIPView.as_view(), name="clear-recommendations"),
 ]
